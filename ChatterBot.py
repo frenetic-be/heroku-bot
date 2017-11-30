bcb64389f2cb934ec2fa1840632d9bd400fd9c75
frenetic-be <github@frenetic.be>
2017-11-30 14:26:34 -0800

    testing env variables

--- ChatterBot.py
+++ ChatterBot.py
@@ -2,39 +2,41 @@
 # import json
 import time
 import tweepy
-import apikeys
+import os
 
-# Twitter API Keys
-consumer_key = apikeys.D_TWITTER_CONSUMER_KEY
-consumer_secret = apikeys.D_TWITTER_CONSUMER_SECRET
-access_token = apikeys.D_TWITTER_ACCESS_TOKEN
-access_token_secret = apikeys.D_TWITTER_ACCESS_TOKEN_SECRET
+print(os.environ['D_TWITTER_CONSUMER_KEY'])
 
-# Twitter credentials
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+# # Twitter API Keys
+# consumer_key = apikeys.D_TWITTER_CONSUMER_KEY
+# consumer_secret = apikeys.D_TWITTER_CONSUMER_SECRET
+# access_token = apikeys.D_TWITTER_ACCESS_TOKEN
+# access_token_secret = apikeys.D_TWITTER_ACCESS_TOKEN_SECRET
 
-
-# Create a function that tweets
-def TweetOut(tweet_number):
-    status = ("Can't stop. Won't stop. Chatting! "
-              f"This is Tweet #{tweet_number}!")
-    # api.update_status(status)
-    print(status)
+# # Twitter credentials
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+# api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
-# Create a function that calls the TweetOut function every minute
-counter = 0
+# # Create a function that tweets
+# def TweetOut(tweet_number):
+#     status = ("Can't stop. Won't stop. Chatting! "
+#               f"This is Tweet #{tweet_number}!")
+#     # api.update_status(status)
+#     print(status)
 
-# Infinitely loop
-while True:
 
-    # Call the TweetQuotes function and specify the tweet number
-    TweetOut(counter)
+# # Create a function that calls the TweetOut function every minute
+# counter = 0
 
-    # Once tweeted, wait 60 seconds before doing anything else
-    time.sleep(60)
+# # Infinitely loop
+# while True:
 
-    # Add 1 to the counter prior to re-running the loop
-    counter = counter + 1
+#     # Call the TweetQuotes function and specify the tweet number
+#     TweetOut(counter)
+
+#     # Once tweeted, wait 60 seconds before doing anything else
+#     time.sleep(60)
+
+#     # Add 1 to the counter prior to re-running the loop
+#     counter = counter + 1
